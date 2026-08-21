2a8d5b8bf56bca6a6c434fc1232b9cb1f388431c
ThibaultRT <36445760+ThibaultRT@users.noreply.github.com>
2026-08-21 16:48:24 +0200

    Use vector PWA icon

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      includeAssets: ['icons/icon.svg'],
       manifest: {
         name: 'Infuse: Evergrowth',
         short_name: 'Evergrowth',
@@ -21,14 +21,10 @@ export default defineConfig({
         scope: repoBase,
         icons: [
           {
-            src: 'icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'icons/icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
           }
         ]
       },
