2f0b4568312644c51647fd529a1413970e5e2fc6
ThibaultRT <36445760+ThibaultRT@users.noreply.github.com>
2026-08-21 18:21:20 +0200

    Add blunt damage hammer icon

---
+++ src/icons.ts
@@ -0,0 +1,5 @@
+export function bluntHammerIcon(size = 12): string {
+  return `<svg class="damage-icon" width="${size}" height="${size}" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+    <path d="M4.8 4.7 7.9 1.6l5.2 5.2-2 2-1.1-1.1-7.3 7.3a2 2 0 0 0 2.8 2.8l7.3-7.3-1.1-1.1 2-2 5.2 5.2-3.1 3.1-2.1-2.1-7.2 7.2a4 4 0 0 1-5.7-5.7L8 7.9 4.8 4.7Z" fill="currentColor"/>
+  </svg>`;
+}
